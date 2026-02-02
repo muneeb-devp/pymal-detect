229bc05e58047ad48fab8d8c24204e0e013dcece
Muneeb Mughal <57582700+muneeb-devp@users.noreply.github.com>
2026-02-02 01:04:12 -0500

    Update default port for application from 8081 to 5000

--- app.py
+++ app.py
@@ -245,5 +245,5 @@ def model_info():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8081))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
